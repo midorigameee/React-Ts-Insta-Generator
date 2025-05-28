998d7aac97a4fd9456bc57d7d5016cce74c3d5be
midorigameee <xqbodp727@gmail.com>
2025-05-28 06:33:32 +0000

    viteに許可するホストを追加する

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    cors: true,
     host: true,
+    allowedHosts: ["react-ts-insta-generator.onrender.com"],
   },
   plugins: [react()],
 });
